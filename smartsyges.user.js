51994c367c844047ad936c55612c7e87c195398d
Matthieu TUDURY <mtudury@expertime.com>
2015-07-24 22:30:37 +0200

    OK basic features done

--- smartsyges.user.js
+++ smartsyges.user.js
@@ -3,19 +3,21 @@
 // @namespace  http://www.expertime.com/
 // @version    0.1
 // @description  SmartSyges
-// @match      https://gestion.expertime.com/sygesweb/SYW_EC_MENUPRINCIPAL/*?WD_ACTION_=MENU&ID=OPM_MO_ACTIVITETEMPSPASSES
+// @match      https://gestion.expertime.com/sygesweb/*
 // @copyright  2015+, M.TUDURY
 // ==/UserScript==
 
-
+'use strict';
 /*  classe smartsyges   */
 
 var smartsyges = smartsyges || {};
 
 // add smart frame button
 smartsyges.init = function () {
+    if ($('form[name="TEM_SA_SAISIEMENSUELLE"]').length > 0) {
         $('#tzLIB_INFPAG').html('<button id="smartsyges_launcher">Launch Smart Frame</button>');
         $('#smartsyges_launcher').click(smartsyges.startframe);
+    }
 }
 
 // construct div or hide it
@@ -26,40 +28,137 @@ smartsyges.startframe = function() {
         return false;
     }
     var k = $('#dwwCELTITREZRP');
+    // contenu de base de la div
     $('<div id="smartsygesframe" style="position:absolute;left:1;top:85;width:998;height:500;z-index:9999;opacity:0.95;background-color:#eeffee;">Selectionner le contrat ci dessous et cliquez sur le jour du mois<br/>Liste des contrats<div id="smartsygeslistcontrat"></div><br/><table></table><button id="smartsyges_apply">Update Syges</button></div>').insertBefore(k);
     
+    // copie le tableau des jours
     $('#smartsygesframe table').html($($('.CSS-LibTitreJourZoneRepetee:first')[0].parentNode).html());
+    
+    // remplis la liste de contrat
     var lstctrdiv = $('#smartsygeslistcontrat');
-    $('input[name$="_AVA_ACTSAI"]').each(function (index, item) { lstctrdiv.append('<input type="radio" name="smartsygesctrselect" id="smartsygesselect'+index+'" value="'+index+'"><label for="smartsygesselect'+index+'" style="background-color: '+smartsyges.colors[index]+';">' + $(item).val()+'</label><button "smartsygesctr'+index+'">ALL</button>' +'<br/>'); });
+    $('input[name$="_AVA_ACTSAI"]').each(function (index, item) { lstctrdiv.append('<input type="radio" name="smartsygesctrselect" id="smartsygesselect'+index+'" value="'+index+'"><label for="smartsygesselect'+index+'" style="background-color: '+smartsyges.colors[index]+';">' + $(item).val()+'</label><button id="smartsygesctr'+index+'" class="smartsygesall" data-ctr="'+index+'">ALL</button>' +'<br/>'); });
     
-    $('#smartsygesframe table td').each(function (index, td) { $(td).attr('data-day', index); $(td).click(smartsyges.updateDay); });
+    var cnt = 1;
+    // rend clickable la zone de calendrier pour gérer l'affectation de contrat
+    $('#smartsygesframe table td').each(function (index, td) { 
+        var std = $(td);
+        if (std.attr('width') > 10) {
+            std.attr('data-day', cnt);
+            if ((std.css('background-color') != 'rgb(212, 212, 212)')&&($('#dzLIB_ABSD'+smartsyges.padDay(cnt)).css('visibility')=='hidden')) {
+                smartsyges.opendays.push(cnt);
+                std.click(smartsyges.updateDay);
+            }
+            else {
+                std.css('background-color', 'rgb(212, 212, 212)');
+            }
+            cnt++;
+        }
+    });
     
+    smartsyges.loadDatas();
+    smartsyges.updateColors();
+    
+    // ajoute le traitement sur les boutons
     $('#smartsyges_apply').click(smartsyges.savevalue);
+    $('.smartsygesall').click(smartsyges.allDays);
     
     return false;
+}
+
+smartsyges.loadDatas = function () {
+    var month = $('#CBO_PERSAI option:selected').text();
+    var k = localStorage.getItem('smartsyges_cra_'+month);
+    if (k) {
+        smartsyges.data = JSON.parse(k);
+    }
 }
 
 smartsyges.updateColors = function () {
-    
+    for( var i = 0; i < smartsyges.opendays.length; i++ ) {
+        var id = smartsyges.opendays[i];
+        var ctrl = smartsyges.getData(id);
+        if (ctrl != '') {
+            $('#smartsygesframe table td[data-day="'+id+'"]').css('background-color', smartsyges.colors[ctrl]);
+        }
+    }
 }
 
+smartsyges.allDays = function () {
+    var ctrall = $(this).attr('data-ctr');
+    for( var i = 0; i < smartsyges.opendays.length; i++ ) {
+        var id = smartsyges.opendays[i];
+        smartsyges.updateDayDatas(id, ctrall);
+    }
+    return false;
+}
+
+smartsyges.updateDayDatas = function (id, ctrl) {
+    $('#smartsygesframe table td[data-day="'+id+'"]').css('background-color', smartsyges.colors[ctrl]);
+    smartsyges.updateData($('#CBO_PERSAI option:selected').text(), id, ctrl);
+}
+
+// user clicked a day
 smartsyges.updateDay = function () {
     var id = $(this).attr('data-day');
     var ctrsel = $('input[name="smartsygesctrselect"]:checked').val();
-    $($('#smartsygesframe table td')[id]).css('background-color', smartsyges.colors[ctrsel]);
-
-    //smartsyges.data
+    if (ctrsel != '') {
+        smartsyges.updateDayDatas(id, ctrsel);
+    }
 }
 
+// let's keep data in case session expired in syges
+smartsyges.updateData = function (month, day, ctr) {
+    var updated = false;
+    for (var i = 0; i < smartsyges.data.length; i++) {
+        var data = smartsyges.data[i];
+        if (data.day == day) {
+            data.ctr = ctr;
+            updated = true;
+        }
+    }
+    if (!updated) {
+        smartsyges.data.push({ day: day, ctr: ctr});
+    }
+    localStorage.setItem('smartsyges_cra_'+month, JSON.stringify(smartsyges.data));
+}
+
+smartsyges.getData = function (day) {
+    for (var i = 0; i < smartsyges.data.length; i++) {
+        var data = smartsyges.data[i];
+        if (data.day == day) {
+            return data.ctr;
+        }
+    }
+    return '';
+}
+
+// applique les donnees dans les tableaux de syges et sauve
 smartsyges.savevalue = function () {
-    alert($('#CBO_PERSAI option:selected').text());
+    for (var i = 0; i < smartsyges.data.length; i++) {
+        var data = smartsyges.data[i];
+        var name = '_' + (parseInt(data.ctr) + 1) + '_AVA_QTES' + smartsyges.padDay(data.day);
+        $('input[name="'+name+'"]').val('1');
+    }
     
-    return false;
+    if ($('#smartsygesframe').length > 0) {
+        var z = $('#smartsygesframe')[0];
+        z.parentNode.removeChild(z);
+        
+        // copied from "Enregistrer ligne" button
+        if(_TEM_SA_SAISIEMENSUELLE_SUB()){_JSL(_PAGE_,'BTN_ENRLIG','_self','','')}
+        
+        return false;
+    }
+}
+
+smartsyges.padDay = function (day) {
+   return ((day< 10)?('0'+day):day);
 }
 
 smartsyges.colors = ['#00FF00', '#FF0000', '#0000FF', '#00FFFF', '#FFFF00', '#FF00FF', '#770077', '#777700', '#007777'];
 
-smartsyges.data = {};
+smartsyges.data = [];
+smartsyges.opendays = [];
 
 // construct help div or hide it
 smartsyges.helpme = function() {
